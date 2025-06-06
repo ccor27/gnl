dc1dc1e9fb8d3049bc7561d7046f06699f387fab
Cristian Camilo Osorio Ramirez <crosorio@c2r2s2.42madrid.com>
2025-06-06 20:43:04 +0200

    error: only_nl

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,20 @@
 #include "get_next_line.h"
 
+void	*free_two(char **ptr1, char **ptr2)
+{
+	if (ptr1 && *ptr1)
+	{
+		free(*ptr1);
+		*ptr1 = NULL;
+	}
+	if (ptr2 && *ptr2)
+	{
+		free(*ptr2);
+		*ptr2 = NULL;
+	}
+	return (NULL);
+}
+
 char	*ft_get_line(char *stash)
 {
 	char	*line;
@@ -18,46 +33,46 @@ char	*ft_get_line(char *stash)
 	return (line);
 }
 
-char	*ft_update_stash(char *stash)
+char	*ft_update_stash(char **stash)
 {
 	int		i;
 	char	*new_stash;
 
+	if (!stash || !*stash)
+		return (NULL);
 	i = 0;
-	while (stash[i] && stash[i] != '\n')
+	while ((*stash)[i] && (*stash)[i] != '\n')
 		i++;
-	if (!stash[i])
+	if (!((*stash)[i]))
 	{
-		free(stash);
+		free(*stash);
 		return (NULL);
 	}
-	new_stash = ft_strdup(stash + i + 1);
-	free(stash);
+	new_stash = ft_strdup(*stash + i + 1);
+	free(*stash);
+	*stash = NULL;
 	return (new_stash);
 }
 
-int	ft_read(int fd, char *stash)
+int	ft_read(int fd, char **stash, char **buffer)
 {
 	int		bytes_read;
 	char	*tmp;
-	char	*buffer;
 
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (0);
 	while (1)
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		bytes_read = read(fd, *buffer, BUFFER_SIZE);
 		if (bytes_read == 0)
 			break ;
 		else if (bytes_read == -1)
-			return (0); // error, free line
-		buffer[bytes_read] = '\0';
-		tmp = ft_strjoin(stash, buffer,0,0);
-		free(stash);
-		stash = tmp;
-		// free tmp ?
-		if (ft_strchr(buffer, '\n'))
+			return (0);
+		(*buffer)[bytes_read] = '\0';
+		tmp = ft_strjoin(*stash, *buffer, 0, 0);
+		if (!tmp)
+			return (0);
+		free(*stash);
+		*stash = tmp;
+		if (ft_strchr(*buffer, '\n'))
 			break ;
 	}
 	return (1);
@@ -67,18 +82,21 @@ char	*get_next_line(int fd)
 {
 	static char	*stash;
 	char		*line;
+	char		*buffer;
 
-	stash = NULL;
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (free_two(&stash,&buffer));// possible error
 	if (fd < 0 || read(fd, 0, 0) < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	if (!ft_read(fd, stash))
-	{
-		free(stash);
-		return (NULL);
-	}
+		return (free_two(&stash,&buffer));
+	if (!ft_read(fd, &stash, &buffer))
+		return (free_two(&stash,&buffer));
+	if (!stash)
+		return (free_two(&stash,&buffer));
 	line = ft_get_line(stash);
 	if (!line)
-		return (NULL); // free ??
-	stash = ft_update_stash(stash);
+		return (free_two(&stash,&buffer));
+	stash = ft_update_stash(&stash);
+	free(buffer);
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -16,7 +16,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2, size_t i, size_t j);
 // get_next_line.c
 char *ft_get_line(char *stash);
-char *ft_update_stash(char *stash);
-int					ft_read(int fd, char *stash);
+char *ft_update_stash(char **stash);
+int	ft_read(int fd, char **stash, char **buffer);
+void	*free_two(char **ptr1, char **ptr2);
 char				*get_next_line(int fd);
 #endif
