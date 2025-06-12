b6e5d940e3308ae22078486a7eacd56dae6f4e11
Cristian Camilo Osorio Ramirez <crosorio@c1r16s6.42madrid.com>
2025-06-12 17:11:51 +0200

    mains added

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crosorio < crosorio@student.42madrid.com>  #+#  +:+       +#+        */
+/*   By: crosorio <crosorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-06-07 13:18:14 by crosorio          #+#    #+#             */
-/*   Updated: 2025-06-07 13:18:14 by crosorio         ###   ########.fr       */
+/*   Created: 2025/06/07 13:18:14 by crosorio          #+#    #+#             */
+/*   Updated: 2025/06/12 17:07:43 by crosorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "get_next_line.h"
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +17,23 @@
 // gcc -Wall -Wextra -Werror main.c get_next_line.c get_next_line_utils.c
 //-o gnl_test
 // valgrind --leak-check=full --show-leak-kinds=all ./gnl_test 1char.txt
-
+/* int	main(int argc, char **argv)
+{
+	int fd;
+	char *line;
+	if(argc != 2)
+		return (1);
+	fd = open(argv[1],O_RDONLY);
+	if(fd < 0)
+		return(1);
+	while((line = get_next_line(999)))
+	{
+		printf("%s",line);
+		free(line);
+	}
+	close(fd);
+	return(0);
+} */
 int	main(int argc, char **argv)
 {
 	int		fd;
--- main_bonus.c
+++ main_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crosorio < crosorio@student.42madrid.com>  #+#  +:+       +#+        */
+/*   By: crosorio <crosorio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-06-12 07:34:53 by crosorio          #+#    #+#             */
-/*   Updated: 2025-06-12 07:34:53 by crosorio         ###   ########.fr       */
+/*   Created: 2025/06/12 07:34:53 by crosorio          #+#    #+#             */
+/*   Updated: 2025/06/12 17:08:08 by crosorio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,56 +17,126 @@
 #include <unistd.h>
 
 #define MAX_TEST_FDS 100
-
-int	main(int argc, char **argv)
+/*
+ int	main(void)
 {
-	int		fds[MAX_TEST_FDS];
-	char	*line;
-	int		active;
-	int		i;
+	int		fd1;
+	int		fd2;
+	char	*line1;
+	char	*line2;
 
-	active = 0;
-	if (argc < 2)
+	fd1 = open("texto1.txt", O_RDONLY);
+	fd2 = open("texto2.txt", O_RDONLY);
+	if (fd1 < 0 || fd2 < 0)
 	{
-		fprintf(stderr, "Uso: %s <archivo1> <archivo2> ...\n", argv[0]);
+		perror("Error al abrir los archivos");
 		return (1);
 	}
-	if (argc - 1 > MAX_TEST_FDS)
+	while (1)
 	{
-		fprintf(stderr, "Máximo de archivos soportados: %d\n", MAX_TEST_FDS);
+		line1 = get_next_line(fd1);
+		line2 = get_next_line(fd2);
+		if(!line1 && !line2)
+			break;
+		if (line1)
+		{
+			printf("FD1: %s", line1);
+			free(line1);
+		}
+		if (line2)
+		{
+			printf("FD2: %s", line2);
+			free(line2);
+		}
+
+	}
+
+	close(fd1);
+	close(fd2);
+	return (0);
+} */
+int	main(void)
+{
+	int		fd1;
+	int		fd2;
+	char	*line1;
+	char	*line2;
+
+	fd1 = open("example.txt", O_RDONLY);
+	fd2 = open("quijote.txt", O_RDONLY);
+	if (fd1 < 0 || fd2 < 0)
+	{
+		perror("Error al abrir los archivos");
 		return (1);
 	}
-	// Abrimos los archivos
-	for (i = 0; i < argc - 1; i++)
+	while ((line1 = get_next_line(fd1)) || (line2 = get_next_line(fd2)))
 	{
-		fds[i] = open(argv[i + 1], O_RDONLY);
-		if (fds[i] < 0)
+		if (line1)
 		{
-			perror("Error al abrir archivo");
-			return (1);
+			printf("FD1: %s", line1);
+			free(line1);
 		}
-	}
-	active = argc - 1;
-	// Leemos línea por línea, alternando FDs
-	while (active > 0)
-	{
-		for (i = 0; i < argc - 1; i++)
+		if (line2)
 		{
-			if (fds[i] == -1)
-				continue ;
-			line = get_next_line(fds[i]);
-			if (line)
-			{
-				printf("%s", line);
-				free(line);
-			}
-			else
-			{
-				close(fds[i]);
-				fds[i] = -1;
-				active--;
-			}
+			printf("FD1: %s", line2);
+			free(line2);
 		}
+
 	}
+
+	close(fd1);
+	close(fd2);
 	return (0);
 }
+// int	main(int argc, char **argv)
+// {
+// 	int		fds[MAX_TEST_FDS];
+// 	char	*line;
+// 	int		active;
+// 	int		i;
+
+// 	active = 0;
+// 	if (argc < 2)
+// 	{
+// 		fprintf(stderr, "Uso: %s <archivo1> <archivo2> ...\n", argv[0]);
+// 		return (1);
+// 	}
+// 	if (argc - 1 > MAX_TEST_FDS)
+// 	{
+// 		fprintf(stderr, "Máximo de archivos soportados: %d\n", MAX_TEST_FDS);
+// 		return (1);
+// 	}
+// 	// Abrimos los archivos
+// 	for (i = 0; i < argc - 1; i++)
+// 	{
+// 		fds[i] = open(argv[i + 1], O_RDONLY);
+// 		if (fds[i] < 0)
+// 		{
+// 			perror("Error al abrir archivo");
+// 			return (1);
+// 		}
+// 	}
+// 	active = argc - 1;
+// 	// Leemos línea por línea, alternando FDs
+// 	while (active > 0)
+// 	{
+// 		for (i = 0; i < argc - 1; i++)
+// 		{
+// 			if (fds[i] == -1)
+// 				continue ;
+// 			line = get_next_line(fds[i]);
+// 			if (line)
+// 			{
+// 				printf("%s", line);
+// 				free(line);
+// 			}
+// 			else
+// 			{
+// 				close(fds[i]);
+// 				fds[i] = -1;
+// 				active--;
+// 			}
+// 		}
+// 	}
+// 	return (0);
+// }
